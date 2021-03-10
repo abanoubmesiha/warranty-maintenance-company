b5c3fd4de906b117b8ad79e9f0a9b5da3ce83530
Abanoub Ayoub <abanoub@terkwaz.com>
2021-03-10 18:08:07 +0200

    comment all code

--- index.js
+++ index.js
@@ -6,33 +6,33 @@ const swaggerJsDoc = require('swagger-jsdoc');
 require('dotenv').config();
 
 
-const options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "warranty-maintenance-company",
-            description: "Engineers and Devices Management System",
-            version: "1.0.0",
-        },
-        servers: [
-            {
-                url: "http://localhost:3000/"
-            }
-        ]
-    },
-    apis: ["./routes/*.js"]
-}
-const specs = swaggerJsDoc(options)
+// const options = {
+//     definition: {
+//         openapi: "3.0.0",
+//         info: {
+//             title: "warranty-maintenance-company",
+//             description: "Engineers and Devices Management System",
+//             version: "1.0.0",
+//         },
+//         servers: [
+//             {
+//                 url: "http://localhost:3000/"
+//             }
+//         ]
+//     },
+//     apis: ["./routes/*.js"]
+// }
+// const specs = swaggerJsDoc(options)
 
 const app = express();
-app.use(bodyParser.json())
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs))
+// app.use(bodyParser.json())
+// app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs))
 
 
 // Import Routes
-const userRoutes = require('./routes/users');
+// const userRoutes = require('./routes/users');
 
-app.use('/users', userRoutes);
+// app.use('/users', userRoutes);
 
 app.use('/', (req, res)=>{
     res.send("Hi there!");
@@ -40,11 +40,11 @@ app.use('/', (req, res)=>{
 
 
 // Connect to DB
-mongoose.connect(
-    // process.env.DB_CONNECTION || 
-    "mongodb+srv://WAR-OWNER:P@ssw0rdWAR@warranty-maintenance-co.c46g7.mongodb.net/development?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    ()=>console.log('Connected to DB!')
-)
+// mongoose.connect(
+//     // process.env.DB_CONNECTION || 
+//     "mongodb+srv://WAR-OWNER:P@ssw0rdWAR@warranty-maintenance-co.c46g7.mongodb.net/development?retryWrites=true&w=majority",
+//     { useNewUrlParser: true, useUnifiedTopology: true },
+//     ()=>console.log('Connected to DB!')
+// )
 
 app.listen(process.env.PORT || 3000, ()=>console.log("Listening to the app!!!"))
