6bc3f63a9780631fc998fd20ba9146a7244104a3
Abanoub Ayoub <abanoub@terkwaz.com>
2021-04-26 13:03:54 +0200

    WAR-09 Install bcrypt and use hashed passwords.

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 
 const UserSchema = mongoose.Schema({
     email: {
@@ -22,5 +23,16 @@ const UserSchema = mongoose.Schema({
         required: false
     }
 });
+
+UserSchema.pre('save', async function(next){
+    try {
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(this.password, salt)
+        this.password = hashedPassword
+        next()
+    } catch (err) {
+        next(err)
+    }
+})
 
 module.exports = mongoose.model('User', UserSchema)
