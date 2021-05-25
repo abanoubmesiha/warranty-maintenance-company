f142392db6369db36ea5c8965e9decf5203270ca
Abanoub Ayoub <abanoub@terkwaz.com>
2021-05-25 18:03:36 +0200

    WAR-12 Enhance Roles error message.

--- util/verify.js
+++ util/verify.js
@@ -4,7 +4,15 @@ const { VerifyTypes } = require('./types/verify-types');
 const User = require('../models/user')
 const Role = require('../models/role')
 
-module.exports = async function(requiredRole, req, res, next){
+const isRoleValid = (requiredRoleName, userRole) => {
+    let res = false;
+    if (requiredRoleName === userRole.name || userRole.name === VerifyTypes.Admin){
+        res = true;
+    } 
+    return res;
+}
+
+module.exports = async function(requiredRoleName, req, res, next){
     const token = req.header('auth-token');
     if (!token){
         next(APIError.unauthorized())
@@ -12,15 +20,19 @@ module.exports = async function(requiredRole, req, res, next){
         try {
             const verified = jwt.verify(token, process.env.TOKEN_SECRET)
             req.user = verified;
-            if (requiredRole === VerifyTypes.Login){
+            if (requiredRoleName === VerifyTypes.Login){
                 next()
             } else {
                 const user = await User.findById(verified)
                 const role = await Role.findById(user.roleId)
-                if (requiredRole === role.name){
+                if (isRoleValid(requiredRoleName, role)){
                     next()
                 } else{
-                    next(APIError.badReq(`${user.name} is ${role.name}, and this action should be done by a ${requiredRole}`))
+                    next(APIError.badReq(`${user.name} is ${role.name}, and this action should be done by ${
+                        requiredRoleName === VerifyTypes.Admin
+                        ?'an Admin'
+                        :'a ' + requiredRoleName
+                    }`))
                 }
             }
         } catch (err) {
